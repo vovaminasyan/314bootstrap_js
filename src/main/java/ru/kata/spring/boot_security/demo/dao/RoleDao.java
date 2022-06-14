d7520823ea1bda50e049766600afbebb7f9ce56d
User <minass1@mail.ru>
2022-06-14 15:15:13 +0700

    end5

---
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/RoleDao.java
@@ -0,0 +1,40 @@
+package ru.kata.spring.boot_security.demo.dao;
+
+import org.springframework.stereotype.Repository;
+import ru.kata.spring.boot_security.demo.model.Role;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class RoleDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public boolean add(Role user) {
+        em.persist(user);
+        return true;
+    }
+
+    public Role findByIdRole(Long id) {
+        return em.find(Role.class, id);
+    }
+
+    public List<Role> listRoles() {
+        return em.createQuery("select s from Role s", Role.class).getResultList();
+    }
+
+    public Role findByName(String name) {
+        return em.createQuery("select u FROM Role u WHERe u.role = :id", Role.class)
+                .setParameter("id", name)
+                .getResultList().stream().findAny().orElse(null);
+    }
+
+    public List<Role> listByName(List<String> name) {
+        return  em.createQuery("select u FROM Role u WHERe u.role in (:id)", Role.class)
+                .setParameter("id", name)
+                .getResultList();
+    }
+
+}
